3e45d289a0f7b82a53ad32b4266ea77b361e4cda
Ryan Tan <ryantan.mail@gmail.com>
2025-04-05 20:02:10 -0700

    skeleton server

--- src/server/csv_query_server.cpp
+++ src/server/csv_query_server.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 #include <iostream>
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
@@ -8,11 +9,29 @@ class CSVQueryServiceImpl final : public csvquery::CSVQueryService::CallbackServ
 	::grpc::ServerUnaryReactor * CSVQueryBorough(::grpc::CallbackServerContext* context, 
 			const ::csvquery::CSVQueryRequest* request, 
 			::csvquery::CSVQueryResponse* response) {
+	
+		response->set_count(1);
+		response->add_results("hello");
+		response->add_results("there");
+		grpc::ServerUnaryReactor* reactor = context->DefaultReactor();
+    reactor->Finish(grpc::Status::OK);
+    return reactor;	
 	}
+	
 };
 
-
+void RunServer() {
+  std::string server_address("0.0.0.0:8888");
+	CSVQueryServiceImpl service;
+	grpc::EnableDefaultHealthCheckService(true);
+	grpc::ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
+}
 
 int main() {
-	std::cout << "hello" << std::endl;
+	RunServer();
 }
